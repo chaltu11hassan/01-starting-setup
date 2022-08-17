c162fc4eba576e196ea02f58e77786dc94d000c5
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-17 11:51:15 -0500

    made changes to app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+
+import ExpenseDate from './ExpenseDate';
+
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) { //props = properties
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+
+      <ExpenseDate date={props.date}></ExpenseDate>
+      
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
