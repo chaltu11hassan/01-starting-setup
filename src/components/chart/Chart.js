97d103480108e5654f9d5f321542b1c8fd3f6f08
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-21 13:43:56 -0500

    chart created

---
+++ src/components/chart/Chart.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./Chart.css";
+
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+    
+    const dataPointValues = props.dataPoints.map((dataPoint)=> dataPoint.value);
+
+    const totalMaximum = math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
